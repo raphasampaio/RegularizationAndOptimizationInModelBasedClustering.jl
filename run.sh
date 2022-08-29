dfe6662238e83d3ffb59c8e366bc8f8168455149
raphasampaio <rsampaio@psr-inc.com>
2022-08-29 14:36:52 -0300

    Updated UnsupervisedLearning.jl

--- run.sh
+++ run.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
 BASE_PATH ="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-unset XPRESSDIR
-unset XPAUTH_PATH
-export XPRESS_JL_NO_DEPS_ERROR=1
-export XPRESS_JL_NO_AUTO_INIT=1
-export XPRESS_JL_SKIP_LIB_CHECK=1
 
-julia --color=yes --project=$BASE_PATH $BASE_PATH/main.jl $@
+$JULIA_167 --color=yes --project=$BASE_PATH $BASE_PATH/main.jl $@
