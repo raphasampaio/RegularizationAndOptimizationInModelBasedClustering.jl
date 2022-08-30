9b84c0e9f78ccc6e8e68905302ea12d0c04ca23f
raphasampaio <rsampaio@psr-inc.com>
2022-08-29 22:34:46 -0300

    Minor changes

--- src/RegularizationAndGlobalOptimizationInModelBasedClustering.jl
+++ src/RegularizationAndGlobalOptimizationInModelBasedClustering.jl
@@ -30,37 +30,37 @@ function run()
     push!(benchmark, UnsupervisedClustering.kmeans_rs)
     push!(benchmark, UnsupervisedClustering.kmeans_hg)
 
-    # push!(benchmark, UnsupervisedClustering.gmm)
-    # push!(benchmark, UnsupervisedClustering.gmm_shrunk)
-    # push!(benchmark, UnsupervisedClustering.gmm_oas)
-    # push!(benchmark, UnsupervisedClustering.gmm_ledoitwolf)
+    push!(benchmark, UnsupervisedClustering.gmm)
+    push!(benchmark, UnsupervisedClustering.gmm_shrunk)
+    push!(benchmark, UnsupervisedClustering.gmm_oas)
+    push!(benchmark, UnsupervisedClustering.gmm_ledoitwolf)
 
-    # push!(benchmark, UnsupervisedClustering.gmm_ms)
-    # push!(benchmark, UnsupervisedClustering.gmm_ms_shrunk)
-    # push!(benchmark, UnsupervisedClustering.gmm_ms_oas)
-    # push!(benchmark, UnsupervisedClustering.gmm_ms_ledoitwolf)
+    push!(benchmark, UnsupervisedClustering.gmm_ms)
+    push!(benchmark, UnsupervisedClustering.gmm_ms_shrunk)
+    push!(benchmark, UnsupervisedClustering.gmm_ms_oas)
+    push!(benchmark, UnsupervisedClustering.gmm_ms_ledoitwolf)
 
-    # push!(benchmark, UnsupervisedClustering.gmm_rs)
-    # push!(benchmark, UnsupervisedClustering.gmm_rs_shrunk)
-    # push!(benchmark, UnsupervisedClustering.gmm_rs_oas)
-    # push!(benchmark, UnsupervisedClustering.gmm_rs_ledoitwolf)
+    push!(benchmark, UnsupervisedClustering.gmm_rs)
+    push!(benchmark, UnsupervisedClustering.gmm_rs_shrunk)
+    push!(benchmark, UnsupervisedClustering.gmm_rs_oas)
+    push!(benchmark, UnsupervisedClustering.gmm_rs_ledoitwolf)
 
-    # push!(benchmark, UnsupervisedClustering.gmm_hg)
-    # push!(benchmark, UnsupervisedClustering.gmm_hg_shrunk)
-    # push!(benchmark, UnsupervisedClustering.gmm_hg_oas)
-    # push!(benchmark, UnsupervisedClustering.gmm_hg_ledoitwolf)
+    push!(benchmark, UnsupervisedClustering.gmm_hg)
+    push!(benchmark, UnsupervisedClustering.gmm_hg_shrunk)
+    push!(benchmark, UnsupervisedClustering.gmm_hg_oas)
+    push!(benchmark, UnsupervisedClustering.gmm_hg_ledoitwolf)
 
     now = Dates.now()
     benchmark_name = "$(Dates.year(now))-$(Dates.month(now))-$(Dates.day(now))_$(Dates.hour(now))-$(Dates.minute(now))-$(Dates.second(now))"
 
     # precompile 
-    # run(benchmark, 100, 3, 2, -0.26, 1)
+    run(benchmark, 100, 3, 2, -0.26, 1)
     clean!(benchmark)
 
     n = 100
-    for k in [3] # 3, 10, 20
-        for c in [-0.26] # -0.26, -0.1, 0.01, 0.21
-            for d in [2] # 2, 5, 10, 20, 30, 40
+    for k in [3, 10, 20] # 3, 10, 20
+        for c in [-0.26, -0.1, 0.01, 0.21] # -0.26, -0.1, 0.01, 0.21
+            for d in [2, 5, 10, 20, 30, 40] # 2, 5, 10, 20, 30, 40
                 for i in 1:20
                     run(benchmark, n, k, d, c, i)
                 end
