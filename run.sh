58ffdf7b43f4180cebd5a0e58a4f0304e8e99d71
raphasampaio <rsampaio@psr-inc.com>
2022-08-29 23:12:10 -0300

    Fixed run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-BASE_PATH ="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BASE_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 $JULIA_167 --color=yes --project=$BASE_PATH $BASE_PATH/main.jl $@
