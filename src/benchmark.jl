00ee474bf5a98b370f2c5fd8623c718e5efeaa3b
raphasampaio <rsampaio@psr-inc.com>
2022-09-14 03:35:13 -0300

    Added wilcoxon

--- src/benchmark.jl
+++ src/benchmark.jl
@@ -1,16 +1,11 @@
 mutable struct Benchmark
-    df::DataFrame
+    syn::DataFrame
     uci::DataFrame
     algorithms::Vector{Function}
 
     function Benchmark()
-        # df = DataFrame()
-        # insertcols!(df, 1, :name => [])
-        # return new(df, Vector{Dataset}(), Vector{Function}())
-
-        df = DataFrame(
+        syn = DataFrame(
             algorithm = Symbol[],
-            n = Int[],
             k = Int[],
             c = Float64[],
             d = Int[],
@@ -27,7 +22,7 @@ mutable struct Benchmark
             ari = Float64[],
             t = Float64[],
         )
-        return new(df, uci, Vector{Function}())
+        return new(syn, uci, Vector{Function}())
     end
 end
 
@@ -37,57 +32,39 @@ function Base.push!(benchmark::Benchmark, algorithm::Function)
 end
 
 function clean!(benchmark::Benchmark)
-    benchmark.df = DataFrame(
-        algorithm = Symbol[],
-        n = Int[],
-        k = Int[],
-        c = Float64[],
-        d = Int[],
-        i = Int[],
-        ari = Float64[],
-        t = Float64[],
-    )
-    benchmark.uci = DataFrame(
-        algorithm = Symbol[],
-        dataset = Symbol[],
-        n = Int[],
-        k = Int[],
-        d = Int[],
-        ari = Float64[],
-        t = Float64[],
-    )
+    deleteat!(benchmark.syn, [1:nrow(benchmark.syn);])
+    deleteat!(benchmark.uci, [1:nrow(benchmark.uci);])
     return nothing
 end
 
-function run(benchmark::Benchmark, n::Int, k::Int, d::Int, c::Float64, i::Int)
+function run(benchmark::Benchmark, k::Int, d::Int, c::Float64, i::Int)
     for algorithm in benchmark.algorithms
-        file = "$(n)_$(k)_$(d)_$(c)_$(i)"
+        file = "$(k)_$(d)_$(c)_$(i)"
         dataset = Dataset(joinpath("data", "$file.csv"))
 
         Random.seed!(1)
         t = @elapsed result = algorithm(dataset.X, dataset.k)
         ari = Clustering.randindex(dataset.expected, result.assignments)[1]
+        # pvalue = HypothesisTests.pvalue(ExactSignedRankTest(dataset.expected, result.assignments))
 
-        # @show algorithm, ari, t
-
-        # save(dataset, result, "D:\\Development\\clustering\\img\\$(n)_$(k)_$(d)_$(c)_$(i)_$(algorithm).png")
-        # println("$(name)_$i\t$current_ari\t$current_elapsed")
-
-        push!(benchmark.df, (Symbol(algorithm), n, k, c, d, i, ari, t))
+        push!(benchmark.syn, (Symbol(algorithm), k, c, d, i, ari, t))
     end
 end
 
 function run(benchmark::Benchmark, file::String)
     for algorithm in benchmark.algorithms
         dataset = Dataset(joinpath("data", "uci", "$file.csv"))
+        n = size(dataset.X, 1)
+        k = dataset.k
+        d = size(dataset.X, 2)
 
         Random.seed!(1)
         t = @elapsed result = algorithm(dataset.X, dataset.k)
         ari = Clustering.randindex(dataset.expected, result.assignments)[1]
+        # pvalue = HypothesisTests.pvalue(ExactSignedRankTest(dataset.expected, result.assignments))
 
         println("$file, $algorithm, $ari, $t")
-
-        push!(benchmark.uci, (Symbol(algorithm), Symbol(file), size(dataset.X, 1), dataset.k, size(dataset.X, 2), ari, t))   
+        push!(benchmark.uci, (Symbol(algorithm), Symbol(file), n, k, d, ari, t))
     end
 end
 
@@ -96,9 +73,9 @@ function save(benchmark::Benchmark, path::String, filename::String)
         mkdir(path)
     end
 
-    if size(benchmark.df, 1) > 0
-        @show benchmark.df
-        CSV.write("$(joinpath(path, "$filename.csv"))", benchmark.df)
+    if size(benchmark.syn, 1) > 0
+        @show benchmark.syn
+        CSV.write("$(joinpath(path, "$filename.csv"))", benchmark.syn)
     end
 
     if size(benchmark.uci, 1) > 0
