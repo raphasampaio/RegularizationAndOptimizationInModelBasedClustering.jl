25f372707bfff9dee2f1f06e990941eeaa64792a
raphasampaio <rsampaio@psr-inc.com>
2022-09-19 02:58:54 -0300

    Minor changes

--- src/postprocessing.jl
+++ src/postprocessing.jl
@@ -1,3 +1,7 @@
+function to_string(x, fmt)
+    return Printf.format(Printf.Format(fmt), x)
+end
+
 function uci()
     results = CSV.read(joinpath("results", "uci.csv"), DataFrame)
     
@@ -92,6 +96,110 @@ function wilcoxon()
 \\end{tabular}
 \\caption{TODO}
 \\end{table}
+""")
+
+end
+
+function cpu_time()
+    algorithms = [
+        "kmeans", 
+        "kmeans_hg", 
+        "gmm", 
+        "gmm_ms", 
+        "gmm_rs", 
+        "gmm_hg", 
+        "gmm_shrunk", 
+        "gmm_ms_shrunk",
+        "gmm_rs_shrunk", 
+        "gmm_hg_shrunk"
+    ]
+
+    formats = Dict(
+        "kmeans" => "%.3f", 
+        "kmeans_hg" => "%.2f",
+        "gmm" => "%.2f",
+        "gmm_ms" => "%.2f",
+        "gmm_rs" => "%.2f",
+        "gmm_hg" => "%.2f",
+        "gmm_shrunk" => "%.2f",
+        "gmm_ms_shrunk" => "%.2f",
+        "gmm_rs_shrunk" => "%.2f",
+        "gmm_hg_shrunk" => "%.2f"
+    )
+
+    results = CSV.read(joinpath("results", "synthetical-v2.csv"), DataFrame)
+    sort!(results, [:algorithm, :k, :c, :d, :i])
+
+println(raw"""
+\begin{table}[htbp]
+\centering
+\scalebox{0.9}
+{
+\begin{tabular}{@{}c|cccccccccc@{}}
+\toprule
+\multirow{3}{*}{k} & k-means & k-means & GMM & GMM & GMM & GMM & GMM & GMM & GMM & GMM \\
+ &  & HG &  & MS & RS & HG &  & MS & RS & HG \\
+ &  &  &  &  &  &  & Shrunk & Shrunk & Shrunk & Shrunk \\ \midrule""")
+
+for k in [3, 10, 20]
+    print("$k ")
+    for algorithm in algorithms
+        df = filter(row -> row.k == k && row.algorithm == algorithm, results)
+        v = to_string(Statistics.mean(df.t), formats[algorithm]) 
+        print("& \$$v\$ ")
+    end
+    println("\\\\")
+end
+println("\\midrule")
+
+print("Avg. ")
+for algorithm in algorithms
+    df = filter(row -> row.algorithm == algorithm, results)
+    v = to_string(Statistics.mean(df.t), formats[algorithm]) 
+    print("& \$$v\$ ")
+end
+println("\\\\ \\bottomrule")
+
+println(raw"""\end{tabular}
+}
+\caption{Average CPU time in seconds, for a different target number of clusters}
+\label{cputime1}
+\end{table}
+
+\begin{table}[htbp]
+\centering
+\scalebox{0.9}
+{
+\begin{tabular}{@{}c|cccccccccc@{}}
+\toprule
+\multirow{3}{*}{d} & k-means & k-means & GMM & GMM & GMM & GMM & GMM & GMM & GMM & GMM \\
+ &  & HG &  & MS & RS & HG &  & MS & RS & HG \\
+ &  &  &  &  &  &  & Shrunk & Shrunk & Shrunk & Shrunk \\ \midrule""")
+
+for d in [2, 5, 10, 20, 30, 40]
+    print("$d ")
+    for algorithm in algorithms
+        df = filter(row -> row.d == d && row.algorithm == algorithm, results)
+        v = to_string(Statistics.mean(df.t), formats[algorithm]) 
+        print("& \$$v\$ ")
+    end
+    println("\\\\")
+end
+println("\\midrule")
+
+print("Avg. ")
+for algorithm in algorithms
+    df = filter(row -> row.algorithm == algorithm, results)
+    v = to_string(Statistics.mean(df.t), formats[algorithm]) 
+    print("& \$$v\$ ")
+end
+println("\\\\ \\bottomrule")
+
+println(raw"""\end{tabular}
+}
+\caption{Average CPU time in seconds, for different number of features}
+\label{cputime2}
+\end{table}
 """)
 
 end
