077bf9b00fea4946c2e6ed0b28541fe55b563c00
raphasampaio <rsampaio@psr-inc.com>
2022-12-28 04:47:24 -0300

    Updated tests

--- test/syn1/run.sh
+++ test/syn1/run.sh
@@ -5,15 +5,15 @@ rm -rf RegularizationAndGlobalOptimizationInModelBasedClustering.jl
 
 git clone https://github.com/raphasampaio/RegularizationAndGlobalOptimizationInModelBasedClustering.jl.git
 
-export JULIA_DOWNLOAD="/tmp/julia/download"
-export JULIA_INSTALL="/tmp/julia/install"
-export JULIA_DEPOT_PATH="/tmp/julia/.julia"
+export JULIA_DOWNLOAD="./julia/download"
+export JULIA_INSTALL="./julia/install"
+export JULIA_DEPOT_PATH="./julia/.julia"
 
 wget https://raw.githubusercontent.com/abelsiqueira/jill/master/jill.sh
 bash jill.sh --no-confirm -v 1.8.4
 
-export PATH=$PATH:"/tmp/julia/install"
-export JULIA_184="/tmp/julia/install/julia"
+export PATH=$PATH:"./julia/install"
+export JULIA_184="./julia/install/julia"
 
 which $JULIA_184
 $JULIA_184 --version
@@ -23,12 +23,12 @@ chmod +x run.sh
 
 ./run.sh -k=[]
 
-./run.sh -i=[3] -c=[-0.26] &
-./run.sh -i=[3] -c=[-0.1]  &
-./run.sh -i=[3] -c=[0.01]  &
-./run.sh -i=[3] -c=[0.21]  &
-./run.sh -i=[4] -c=[-0.26] &
-./run.sh -i=[4] -c=[-0.1]  &
-./run.sh -i=[4] -c=[0.01]  &
-./run.sh -i=[4] -c=[0.21]  &
+./run.sh -i=[1] -c=[-0.26] &
+./run.sh -i=[1] -c=[-0.1]  &
+./run.sh -i=[1] -c=[0.01]  &
+./run.sh -i=[1] -c=[0.21]  &
+./run.sh -i=[2] -c=[-0.26] &
+./run.sh -i=[2] -c=[-0.1]  &
+./run.sh -i=[2] -c=[0.01]  &
+./run.sh -i=[2] -c=[0.21]  &
 wait
---
+++ test/uci11/run.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+rm -rf RegularizationAndGlobalOptimizationInModelBasedClustering.jl
+
+git clone https://github.com/raphasampaio/RegularizationAndGlobalOptimizationInModelBasedClustering.jl.git
+
+export JULIA_DOWNLOAD="./julia/download"
+export JULIA_INSTALL="./julia/install"
+export JULIA_DEPOT_PATH="./julia/.julia"
+
+wget https://raw.githubusercontent.com/abelsiqueira/jill/master/jill.sh
+bash jill.sh --no-confirm -v 1.8.4
+
+export PATH=$PATH:"./julia/install"
+export JULIA_184="./julia/install/julia"
+
+which $JULIA_184
+$JULIA_184 --version
+
+cd RegularizationAndGlobalOptimizationInModelBasedClustering.jl
+chmod +x run.sh
+
+./run.sh -k=[]
+
+./run.sh --algorithm=[1,4,5,6,7,8,9,10,11,12] -i=[11] -k=[] --uci --datasets=[14] &
+./run.sh --algorithm=[1,4,5,6,7,8,9,10,11,12] -i=[12] -k=[] --uci --datasets=[14] &
+./run.sh --algorithm=[1,4,5,6,7,8,9,10,11,12] -i=[13] -k=[] --uci --datasets=[14] &
+./run.sh --algorithm=[1,4,5,6,7,8,9,10,11,12] -i=[14] -k=[] --uci --datasets=[14] &
+./run.sh --algorithm=[1,4,5,6,7,8,9,10,11,12] -i=[15] -k=[] --uci --datasets=[14] &
+./run.sh --algorithm=[1,4,5,6,7,8,9,10,11,12] -i=[16] -k=[] --uci --datasets=[14] &
+./run.sh --algorithm=[1,4,5,6,7,8,9,10,11,12] -i=[17] -k=[] --uci --datasets=[14] &
+./run.sh --algorithm=[1,4,5,6,7,8,9,10,11,12] -i=[18] -k=[] --uci --datasets=[14] &
+./run.sh --algorithm=[1,4,5,6,7,8,9,10,11,12] -i=[19] -k=[] --uci --datasets=[14] &
+./run.sh --algorithm=[1,4,5,6,7,8,9,10,11,12] -i=[20] -k=[] --uci --datasets=[14] &
+wait
