2fe8eb672373facaebb30baf60503da40f6b7021
raphasampaio <rsampaio@psr-inc.com>
2023-12-02 02:17:57 -0300

    Updated

--- run.sh
+++ run.sh
@@ -6,7 +6,4 @@ wget https://julialang-s3.julialang.org/bin/linux/x64/1.9/julia-1.9.4-linux-x86_
 tar -xvzf julia-1.9.4-linux-x86_64.tar.gz
 
 export JULIA_DEPOT_PATH="./julia-env"
-
-
-sleep 5d
 ./julia-1.9.4/bin/julia --color=yes --project=$BASE_PATH $BASE_PATH/main.jl $@
