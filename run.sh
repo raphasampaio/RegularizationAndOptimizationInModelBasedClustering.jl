f5bca1b1eebe5f6e514a5904c8e362641c963860
raphasampaio <rsampaio@psr-inc.com>
2023-12-18 14:13:03 -0300

    Project updates

--- run.sh
+++ run.sh
@@ -7,5 +7,3 @@ tar -xvzf julia-1.9.4-linux-x86_64.tar.gz
 
 export JULIA_DEPOT_PATH="./julia-env"
 ./julia-1.9.4/bin/julia --color=yes --project=$BASE_PATH $BASE_PATH/main.jl $@
-
-$JULIA_194 --color=yes --project=$BASE_PATH $BASE_PATH/main.jl $@
