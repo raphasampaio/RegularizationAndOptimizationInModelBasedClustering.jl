7bc08ddb08e642a67824d5a13059f748e50ec77d
raphasampaio <rsampaio@psr-inc.com>
2023-12-19 00:56:35 -0300

    Project updates

---
+++ benchmark/syn7/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+rm -rf RegularizationAndGlobalOptimizationInModelBasedClustering.jl
+
+git clone -b feature/julia185 https://github.com/raphasampaio/RegularizationAndGlobalOptimizationInModelBasedClustering.jl.git
+
+cd RegularizationAndGlobalOptimizationInModelBasedClustering.jl
+chmod +x run.sh
+
+./run.sh -i=[13,14]
